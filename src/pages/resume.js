a9320a6db5feb5b99ce0309c7922b6bad524b304
Jeehyae dance <jeehyae91@gmail.com>
2020-04-11 22:36:30 -0700

    created work page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -22,7 +21,6 @@ const Resume = () => (
       </h4>
       <ResumeImg />
     </div>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Work = () => (
+  <Layout>
+    <SEO title="Work" />
+    <div>hello from work page</div>
+  </Layout>
+)
+
+export default Work
