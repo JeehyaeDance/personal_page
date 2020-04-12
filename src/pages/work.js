37c7fcf9297e092220b5edd01afdf5a1b5abcbbe
Jeehyae dance <jeehyae91@gmail.com>
2020-04-11 23:27:40 -0700

    added style sheet for work page and demo gif

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,11 +2,30 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "./work.module.css"
+import climbingDemo from "../gifs/climbing-day-demo.gif"
 
 const Work = () => (
   <Layout>
     <SEO title="Work" />
-    <div>hello from work page</div>
+    <div className={styles.workPage}>
+      <div className={styles.work}>
+        <img
+          className={styles.demo}
+          src={climbingDemo}
+          alt="climbing day demo"
+        />
+        <div className={styles.workSpec}>
+          <h3>Climbing Day</h3>
+          <p>
+            Climbing Day is a web application. It is a rock climbing journal
+            that allows users to log their rock climbing activity and track the
+            improvement with weekly analytics. The backend server can handle and
+            route data to both the web application and mobile application.
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
