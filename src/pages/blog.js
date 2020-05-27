0b9eb13694f754e77bbc5036b9ca2d132a71cc96
Jeehyae dance <jeehyae91@gmail.com>
2020-05-26 23:25:42 -0700

    add blog

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Blog = () => (
+  <Layout>
+    <SEO title="blog" />
+    <h1 style={{ textAlign: "center", marginTop: "150px", fontSize: "100px" }}>
+      Coming Soon!🤟🏼
+    </h1>
+  </Layout>
+)
+
+export default Blog
